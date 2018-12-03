fed5cc6cb398563e3dc75d4a84dfaf88cf129ffc
donjoe <giumontanaro@gmail.com>
2018-12-03 16:15:36 +0100

    Handle logic for play current round

--- js/app.js
+++ js/app.js
@@ -1,10 +1,38 @@
+/* DATA MODEL */
+const data = {
+    currentRound: 1,
+    randomActiveButtons: [],
+    userAttempts: []
+};
+
+/* CONTROLLER */
 const controller = {
+
   init: function() {
     view.cacheDOM();
     view.bindEvents();
+    this.playCurrentRound();
+  },
+
+  getCurrentRound: function() {
+    return data.currentRound;
+  },
+
+  playCurrentRound: function() {
+    data.randomActiveButtons = [];
+    data.userAttempts = [];
+    const round = this.getCurrentRound();
+
+    //i.e round 2 -> 2 elements must be guessed. round 4 -> 4 elements must be guessed ecc...
+    for(let i = 1; i <= round; i++) {
+       setTimeout(() => {
+         console.log(i)
+       }, 800 * i);
+    }
   }
 }
 
+/* VIEW */
 const view = {
   cacheDOM: function() {
     this.buttons = document.querySelectorAll(".innerContainer div");
